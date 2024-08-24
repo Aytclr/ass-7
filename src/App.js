fe8f2a9e31b9091b364ac37bc7fd96f904619b9a
AYTC <aytacalir@gmail.com>
2024-08-24 17:50:17 +0300

    new start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import "./style/style.css";
-import Pictures from "./components/Pictures";
-import HomePage from "./pages/Homepage"
+
 
 function App() {
   return (
     
       <div>
-        <HomePage/>
-        <Pictures/>
+        
       </div>
     
   );
